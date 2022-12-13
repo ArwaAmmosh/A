9eec7b814c1624ca493433d4e56b23bae331071b
ArwaAmmosh <112775694+ArwaAmmosh@users.noreply.github.com>
2022-12-13 20:38:00 +0200

    Update A.cs

--- A.cs
+++ A.cs
@@ -8,4 +8,25 @@ public person (string name,int age)
 Name=name;
 Age=age;
 }
+public class student: person { public int Year;
+public float Gpa;
+public override void print (){
+Console.WriteLine("My name is {Name}, my age is {Age},and Gpa is{Gpa}");
+
+}
+Public class database {
+Public int index;
+public person[] people =new person[50];
+public void add(Student student){
+people [index]=student;
+index++;
+}
+}
+
+
+}
+}
+public class program {
+public static void Main(){
+}
 }
