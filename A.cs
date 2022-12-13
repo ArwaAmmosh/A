968dee94d8df6dbaaaf840df85fc2195a17214ca
ArwaAmmosh <112775694+ArwaAmmosh@users.noreply.github.com>
2022-12-13 20:54:37 +0200

    Update A.cs

--- A.cs
+++ A.cs
@@ -3,19 +3,22 @@ namespace oops;
 public abstract class person{
 public string Name;
 public int Age;
-public person (string name,int age)
+public int Year;
+public float Gpa;
+public person (string name,int age,int Year,int gpa,)
 {
 Name=name;
 Age=age;
+Year=year;
+Gpa=gpa;
 }
-public class student: person { public int Year;
-public float Gpa;
+public class student: person { 
 public override void print (){
 Console.WriteLine("My name is {Name}, my age is {Age},and Gpa is{Gpa}");
 
 }
 Public class database {
-Public int index;
+private int index;
 public person[] people =new person[50];
 public void add(Student student){
 people [index]=student;
@@ -28,5 +31,12 @@ index++;
 }
 public class program {
 public static void Main(){
+var data=new database();
+var name=Console.ReadLine();
+var age =Convert.ToInt32(Console.ReadLine());
+var year =Convert.ToInt32(Console.ReadLine());
+var gpa=Convert.ToSingle(Console.ReadLine());
+var student=new Student (name,age,year,Gpa);
+data.add(student);
 }
 }
