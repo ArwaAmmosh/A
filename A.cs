72fc72308de8e4732dd823c9b023973a86d2d6c8
ArwaAmmosh <112775694+ArwaAmmosh@users.noreply.github.com>
2022-12-14 16:23:32 +0200

    Update A.cs

--- A.cs
+++ A.cs
@@ -1,42 +1,99 @@
+
 using System;
-namespace oops;
+namespace ObjectOriented;
 public abstract class person{
-public string Name;
-public int Age;
-public int Year;
-public float Gpa;
-public person (string name,int age,int Year,int gpa,)
-{
-Name=name;
-Age=age;
-Year=year;
-Gpa=gpa;
+    public string Name;
+    public int Age;
+    public person (string name, int age){
+        Name =name;
+        Age =age;
+    }
+    public abstract void print();
+    
 }
-public class student: person { 
-public override void print (){
-Console.WriteLine("My name is {Name}, my age is {Age},and Gpa is{Gpa}");
+public class Student :person {
+    public int Year;
+    public float Gpa;
+    public Student (string name,int age ,int year,float gpa) :base(name,age){
+        Year =year;
+        Gpa=gpa;
+    }
+    public override void print (){
+        Console.WriteLine($"My name is {Name},my age is {Age},and gpa is {Gpa}");
+            }
 
 }
-Public class database {
-private int index;
-public person[] people =new person[50];
-public void add(Student student){
-people [index]=student;
-index++;
-}
-}
+public class Staff :person {
+    public double Salary;
+    public int Joinyear;
+    public Staff (string name,int age ,double salary,int joinyear) :base(name,age){
+        Salary=salary;
+        Joinyear=joinyear;
+    }
+    public override void print (){
+        Console.WriteLine($"My name is {Name},my age is {Age},and salary is {Salary}");
+            }
 
-
 }
+public class Database{
+    private int index;
+    public person[] people=new person[50];
+    public void addstudent(Student student){
+       people[index]=student;
+         index++;
+    } 
+     public void addstaff(Staff staff){
+       people[index]=staff;
+         index++;
+    } 
+    
+    public  void print(){
+        for(int i=0;i<=index;i++){
+
+           people[i].print();
+        }
+    }  
+   
 }
 public class program {
-public static void Main(){
-var data=new database();
-var name=Console.ReadLine();
-var age =Convert.ToInt32(Console.ReadLine());
-var year =Convert.ToInt32(Console.ReadLine());
-var gpa=Convert.ToSingle(Console.ReadLine());
-var student=new Student (name,age,year,Gpa);
-data.add(student);
-}
-}
+    public static void Main (){
+        Database data=new Database();
+        while (true)
+        {Console.WriteLine("Enter \n1 to add a student\n2 to add a staff \n3 to print out all people stored");
+        var n=Convert.ToInt32(Console.ReadLine());
+        if(n==1)
+        {   Console.WriteLine("Name :");
+            var name=Console.ReadLine();
+            Console.WriteLine("Age :");
+            var age=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Year :");
+            var year=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Gpa :");
+            var gpa=Convert.ToSingle(Console.ReadLine());
+            var student=new Student(name,age,year,gpa);
+            data.addstudent(student);
+            Console.WriteLine("Stored successfully");
+
+        }
+        else if(n==2)
+        {   Console.WriteLine("Name :");
+            var name=Console.ReadLine();
+            Console.WriteLine("Age :");
+            var age=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Salary :");
+            var salary=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Join Year :");
+            var joinyear=Convert.ToInt32(Console.ReadLine());
+            var staff=new Staff(name,age,salary,joinyear);
+            data.addstaff(staff);
+           Console.WriteLine("Stored successfully");
+
+
+        } 
+         else if (n==3){
+         data.print();
+    
+         }}
+       
+    }
+    }
